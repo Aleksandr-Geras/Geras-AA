1093c83caf92af89c5ebfc41eb0572cc07382208
Aleksandr-Geras <gerasimenko.aleksandr.97@mail.ru>
2025-06-16 21:02:38 +0700

    new button

--- src/onlyjs/script.js
+++ src/onlyjs/script.js
@@ -16,4 +16,24 @@ function showMore() {
   }
 }
 
+function showMoreSecond() {
+  const containerBrands = document.querySelector('.swiper_second');
+  const btnTxt = document.querySelector('.show-more-btn-second');
+  const isExpanded = containerBrands.style.height === '170px';
+  if (isExpanded) {
+    containerBrands.style.height = '';
+    btnTxt.innerHTML = 'Скрыть';
+
+    btnTxt.style.backgroundImage = 'url(../img/arrows/icon.svg)';
+    btnTxt.style.backgroundPosition = '15%, 35%';
+  } else {
+    containerBrands.style.height = '170px';
+    btnTxt.innerHTML = 'Показать все';
+    btnTxt.style.backgroundImage = 'url(../img/arrows/expand.svg)';
+    btnTxt.style.backgroundPosition = '-5%, 40%';
+  }
+}
+
 document.getElementById('smBtn').addEventListener('click', showMore);
+
+document.getElementById('BtnSecond').addEventListener('click', showMoreSecond);
